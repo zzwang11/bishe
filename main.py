028a04ed439b3c4ce02919d2d5a61e6eaa850e77
wangan67 <n_3uK!WJ$C>
2020-10-13 10:46:07 +0800

    10.13

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow,QFileDialog
 from MainWindow import Ui_MainWindow
 from PyQt5.QtCore import QCoreApplication
 from openfile import MyWindow
@@ -10,7 +10,13 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
         self.pushButton_5.clicked.connect(QCoreApplication.instance().quit)
-        # self.pushButton_4.clicked.connect(MyWindow.msg())
+        self.pushButton_4.clicked.connect(self.save_file())
+
+    def save_file(self):
+        fileName2, ok2 = QFileDialog.getSaveFileName(self,
+                                                     "文件保存",
+                                                     "./",
+                                                     "All Files (*);;Text Files (*.txt)")
 
 
 if __name__ == "__main__":
